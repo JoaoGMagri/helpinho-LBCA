e4feecdf552b3ec146e0ca4d4f2f875dc73bc9e7
jgmagri <jg2durigon@gmail.com>
2024-08-20 05:06:39 -0400

    feat: adiciona cards

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -2,13 +2,33 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { BannerApresentationComponent } from '../../components/banner-apresentation/banner-apresentation.component';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, BannerApresentationComponent],
+  imports: [HeaderComponent, FooterComponent, BannerApresentationComponent, CardComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  instructions = [
+    {
+      title: "Escolha",
+      description: "Escolha um helpinho que deseja ajudar"
+    },
+    {
+      title: "Escolha",
+      description: "Escolha um helpinho que deseja ajudar"
+    },
+    {
+      title: "Escolha",
+      description: "Escolha um helpinho que deseja ajudar"
+    },
+    {
+      title: "Escolha",
+      description: "Escolha um helpinho que deseja ajudar"
+    }
+  ]
 
 }
