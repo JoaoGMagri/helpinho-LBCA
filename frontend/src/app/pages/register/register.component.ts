1d232eb066e39f8d86927abfe4abd6486a74c613
jgmagri <jg2durigon@gmail.com>
2024-08-21 18:47:28 -0400

    feat: captura campos de input da pagina de registro

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
 
+  httpClient = inject(HttpClient)
+  router = inject(Router)
+
+
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required] ),
+    email: new FormControl('', [Validators.required, Validators.email] ),
+    number: new FormControl('', [Validators.required] ),
+    date: new FormControl('', [Validators.required] ),
+    password: new FormControl('', [Validators.required] ),
+  })
+
+  
+  onSubmit() {
+    const isFormValid = this.form.valid
+
+    if(isFormValid){
+      let teste = {
+        name: this.form.controls.name.value,
+        email: this.form.controls.email.value,
+        number: this.form.controls.number.value,
+        date: this.form.controls.date.value,
+        password: this.form.controls.password.value
+      };
+
+      this.httpClient.post('api/user', teste).subscribe(() => {
+        this.router.navigateByUrl("/login")
+      });
+
+    }
+  }
 }
