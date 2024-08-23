a4be53a7f5a8c509580c0f7a6ceaa34475ebc026
jgmagri <jg2durigon@gmail.com>
2024-08-22 21:35:16 -0400

    feat: tabelas novas e dados mocados no bd temporario

--- my-serverless-project/src/config/database.ts
+++ my-serverless-project/src/config/database.ts
@@ -1,16 +1,135 @@
+import { Help, Session, User } from "./type";
 
 
 export const db: userTyper = {
     user: [],
-    help:[],
+    help:[
+        {
+          title:"Comprar um PS5",
+          decription:"Meu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novoMeu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novo",
+          type: "Games",
+          image: "assets/usuario.jpg",
+          author: { 
+            name: "Olivia Rhye", 
+            email: "olivia@untitledui.com", 
+            image: "assets/usuario.jpg",
+          },
+          urgent:true,
+          supporters: []
+        },
+        {
+          title:"Comprar um PS6",
+          decription:"Meu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novoMeu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novo",
+          type: "Literatura",
+          image: "assets/usuario.jpg",
+          author: { 
+            name: "Olivia Rhye", 
+            email: "olivia@untitledui.com", 
+            image: "assets/usuario.jpg",
+          },
+          urgent:false,
+          supporters: []
+        },
+        {
+          title:"Comprar um PS7",
+          decription:"Meu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novoMeu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novo",
+          type: "Doença",
+          image: "assets/usuario.jpg",
+          author: { 
+            name: "Olivia Rhye", 
+            email: "olivia@untitledui.com", 
+            image: "assets/usuario.jpg",
+          },
+          urgent:false,
+          supporters: []
+        },
+        {
+          title:"Comprar um PS7",
+          decription:"Meu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novoMeu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novo",
+          type: "Doença",
+          image: "assets/usuario.jpg",
+          author: { 
+            name: "Olivia Rhye", 
+            email: "olivia@untitledui.com", 
+            image: "assets/usuario.jpg", 
+          },
+          urgent:false,
+          supporters: ["assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg"]
+        },
+        {
+          title:"Comprar um PS7",
+          decription:"Meu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novoMeu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novo",
+          type: "Doença",
+          image: "assets/usuario.jpg",
+          author: { 
+            name: "Olivia Rhye", 
+            email: "olivia@untitledui.com", 
+            image: "assets/usuario.jpg", 
+          },
+          urgent:true,
+          supporters: ["assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg"]
+        },
+        {
+          title:"Comprar um PS7",
+          decription:"Meu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novoMeu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novo",
+          type: "Doença",
+          image: "assets/usuario.jpg",
+          author: { 
+            name: "Olivia Rhye", 
+            email: "olivia@untitledui.com", 
+            image: "assets/usuario.jpg" 
+          },
+          urgent:false,
+          supporters: ["assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg"],
+        },
+        {
+          title:"Comprar um PS7",
+          decription:"Meu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novoMeu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novo",
+          type: "Doença",
+          image: "assets/usuario.jpg",
+          author: { 
+            name: "Olivia Rhye", 
+            email: "olivia@untitledui.com", 
+            image: "assets/usuario.jpg" 
+          },
+          urgent:false,
+          supporters: ["assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg"],
+        },
+        {
+          title:"Comprar um PS7",
+          decription:"Meu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novoMeu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novo",
+          type: "Doença",
+          image: "assets/usuario.jpg",
+          author: { 
+            name: "Olivia Rhye", 
+            email: "olivia@untitledui.com", 
+            image: "assets/usuario.jpg", 
+          },
+          urgent:false,
+          supporters: ["assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg","assets/usuario.jpg"]
+        },
+        {
+          title:"Comprar um PS7",
+          decription:"Meu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novoMeu PS2 queimou e preciso comprar um novo, falta R$5000 para comprar um novo",
+          type: "Games",
+          image: "assets/usuario.jpg",
+          author: { 
+            name: "Olivia Rhye", 
+            email: "olivia@untitledui.com", 
+            image: "assets/usuario.jpg",
+          },
+          urgent:true,
+          supporters: []
+        }
+    ],
     donation:[],
     session:[]
 };
 
 type userTyper = {
-    user: any[]
+    user: User[]
     help: any[]
     donation: any[]
-    session: any[]
+    session: Session[]
 }
 
--- my-serverless-project/src/config/type.ts
+++ my-serverless-project/src/config/type.ts
@@ -1,7 +1,8 @@
+import { ulid } from "ulid";
 import { db } from "./database";
 
 export class User {
-    id: Number;
+    id: string;
     name: string;
     email: string;
     password: string;
@@ -12,7 +13,7 @@ export class User {
     updated: Date;
 
     constructor(data: any) {
-        this.id = db.user.length;
+        this.id = ulid();
         this.name = data.name;
         this.email = data.email;
         this.password = data.password;
@@ -21,5 +22,53 @@ export class User {
         this.number = data.number
         this.created = new Date();
         this.updated = new Date();
+    }
+}
+
+export class Help {
+    id: string;
+    title: string;
+    description: string;
+    type: string;
+    image: string;
+    author: Number;
+    urgent: boolean;
+    supportes: string[];
+    created: Date;
+    updated: Date;
+
+    constructor(data:any){
+        this.id = ulid();
+        this.title = data.title;
+        this.description = data.description;
+        this.type = data.type;
+        this.image = data.image
+        this.author = data.
+        this.urgent = data.urgent
+        this.supportes = []
+        this.created = new Date();
+        this.updated = new Date();
+    }
+
+
+
+
+}
+
+export class Session {
+    id: string;
+    userId: string;
+    name: string;
+    email: string;
+    image: string;
+    token: string;
+
+    constructor(data:any) {
+        this.id = ulid();
+        this.userId = data.userId;
+        this.name = data.name;
+        this.email = data.email;
+        this.image = data.image;
+        this.token = data.token;
     }
 }
