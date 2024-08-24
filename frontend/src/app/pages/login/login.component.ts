f13935ae3315bd0e3aa089bed7dab61b5c8b2ed5
jgmagri <jg2durigon@gmail.com>
2024-08-24 09:02:34 -0400

    feat: login funcional

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { StorageService } from '../../service/storage.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  
+  httpClient = inject(HttpClient);
+  router = inject(Router);
+  storage = inject(StorageService);
 
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email] ),
+    password: new FormControl('', [Validators.required] ),
+  })
+
+  onSubmit() {
+    if(this.form.valid){
+      let body = {
+        email: this.form.controls.email.value,
+        password: this.form.controls.password.value
+      };
+      this.httpClient.post('api/auth', body).subscribe((res) => {
+        this.storage.saveStorage(res);
+        this.router.navigateByUrl("/")
+      });
+    } 
+  }
 }
